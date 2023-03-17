61be457e1b59b91414a3868eeb49c2ded9e350c2
nishu-murmu <nishumurmu017@gmail.com>
2023-03-17 20:49:33 +0530

    refactoring logic

--- album.js
+++ album.js
@@ -1,9 +1,10 @@
-let list;
 let totalList = [];
-let sumlist = [];
 let start = 0;
 let innerhtml = "";
 let size = 10;
+let isLoadMore = false;
+let isSearch = false;
+let isFilter = false;
 let blogCard = document.getElementsByClassName("blog-card");
 let blogSection = document.getElementById("blog-section");
 let skeletonCard = document.getElementsByClassName("blog-card-test");
@@ -11,6 +12,7 @@ let radialLoader = document.getElementById("loader");
 let loadMoreButton = document.getElementById("load-more");
 let inputElem = document.getElementById("search-text");
 let filterElem = document.getElementById("filters");
+let clearButton = document.getElementById("clear");
 
 // common functions
 const removeLoader = () => {
@@ -31,73 +33,96 @@ const showSkeleton = () => {
   }
 };
 
-const getHTML = (url, title, id) => {
-  return `
-            <div class="blog-card" id="blog-card">
-              <div class="blog-img">
-                <img
-                  id="blog-img"
-                  src="${url}"
-                  alt="samosa"
-                />
-              </div>
-              <div class="card-body">
-                <p id="blog-description" class="card-description">
-                    ${title}
-                </p>
-                <div class="card-buttons">
-                  <div class="button-wrapper">
-                    <button class="button button-outline">${id}</button>
-                    <button class="button button-outline">Edit</button>
-                  </div>
-                    <p class="time-line"><small>${new Date().toLocaleTimeString()}</small></p>
-                </div>
-              </div>
-            </div>
-`;
-};
-
-const injectHTML = (arr, limit, inputValue, filtervalue) => {
+const getHTML = (arr) => {
   innerhtml = "";
-  let updatearr = [];
   arr.forEach((item) => {
-    if (limit && item.id <= limit) {
-      innerhtml += getHTML(item.url, item.title, item.albumId);
-    }
-    if (inputValue && item.title.includes(inputValue.toLowerCase())) {
-      innerhtml += getHTML(item.url, item.title, item.albumId);
-    }
-    if (filtervalue && item.albumId == filtervalue) {
-      updatearr.push(item);
-      innerhtml += getHTML(item.url, item.title, item.albumId);
-    }
+    innerhtml += `
+    <div class="blog-card" id="blog-card">
+      <div class="blog-img">
+        <img
+          id="blog-img"
+          src="${item.url}"
+          alt="samosa"
+        />
+      </div>
+      <div class="card-body">
+        <p id="blog-description" class="card-description">
+            ${item.title}
+        </p>
+        <div class="card-buttons">
+          <div class="button-wrapper">
+            <button class="button button-outline">${item.albumId}</button>
+            <button class="button button-outline">${item.id}</button>
+          </div>
+            <p class="time-line"><small>${new Date().toLocaleTimeString()}</small></p>
+        </div>
+      </div>
+    </div>
+`;
   });
-  sumlist.push(...sumlist, ...updatearr);
-  sumlist = [...new Map(sumlist.map((item) => [item["id"], item])).values()];
   blogSection.innerHTML = innerhtml;
 };
 
-const removeHTML = () => {
-  while (blogSection.hasChildNodes()) {
-    blogSection.firstChild.remove();
+const getFilters = (
+  arr,
+  inputValue,
+  filtervalue,
+  isLoadMore,
+  isSearch,
+  isFilter,
+  limit
+) => {
+  if (isSearch) {
+    arr = arr.filter((item) =>
+      item.title.toLowerCase().includes(inputValue.toLowerCase())
+    );
   }
+
+  if (isFilter) {
+    arr = arr.filter((item) => {
+      return item.albumId === parseInt(filtervalue);
+    });
+  }
+
+  if (isFilter && isSearch) {
+    arr = arr.filter((item) => {
+      return (
+        item.title.toLowerCase().includes(inputValue.toLowerCase()) &&
+        item.albumId === parseInt(filtervalue)
+      );
+    });
+  }
+  arr = arr.slice(start, limit);
+
+  if (arr.length < 10) {
+    loadMoreButton.classList.add("hidden");
+  }
+
+  return arr;
 };
 
 // functions
-function getPhotos(start, length, filterValue) {
+function getPhotos() {
   showSkeleton();
+  inputElem.value = "";
 
   setTimeout(async () => {
     await fetch("https://jsonplaceholder.typicode.com/photos")
       .then((res) => res.json())
       .then((data) => {
-        list = data.slice(start, length);
         totalList = data;
       });
-    sumlist.push(...sumlist, ...list);
 
-    sumlist = [...new Map(sumlist.map((item) => [item["id"], item])).values()];
-    injectHTML(sumlist, length, "", filterValue);
+    const resultArray = getFilters(
+      totalList,
+      inputElem.value,
+      filterElem.value,
+      isLoadMore,
+      isSearch,
+      isFilter,
+      size
+    );
+    getHTML(resultArray);
 
     for (let i = 0; i < blogCard.length; ++i) {
       blogCard[i].classList.remove("hidden");
@@ -109,32 +134,30 @@ function getPhotos(start, length, filterValue) {
   }, 2100);
 }
 
-function getMorePhotos(start, end) {
-  if (start !== undefined && end !== undefined) {
-    totalList.forEach((item) => {
-      if (item.id >= start && item.id <= end) {
-        sumlist.push(item);
-      }
-    });
-  }
-  injectHTML(sumlist, length, "", filterElem.value, true);
-}
-
-const search = () => {
-  let inputValue = document.getElementById("search-text").value;
-  if (inputValue !== "") {
-    removeHTML();
-    innerhtml = "";
-    injectHTML(sumlist, length, inputValue, 0);
-  } else {
-    removeHTML();
-    getMorePhotos();
-  }
+const search = (inputValue, isSearch) => {
+  const resultArray = getFilters(
+    totalList,
+    inputValue,
+    filterElem.value,
+    isLoadMore,
+    isSearch,
+    isFilter,
+    size
+  );
+  getHTML(resultArray);
 };
 
-const filter = (value) => {
-  innerhtml = "";
-  injectHTML(totalList, length, "", value);
+const filter = (value, isFilter) => {
+  const resultArray = getFilters(
+    totalList,
+    inputElem.value,
+    value,
+    isLoadMore,
+    isSearch,
+    isFilter,
+    size
+  );
+  getHTML(resultArray);
 };
 
 // function to get photos from the api
@@ -142,13 +165,43 @@ getPhotos(0, size);
 
 // function to load more photos
 loadMoreButton.onclick = () => {
-  getMorePhotos((start += 10), (size += 10));
+  isLoadMore = true;
+  size += 10;
+  const resultArray = getFilters(
+    totalList,
+    inputElem.value,
+    filterElem.value,
+    isLoadMore,
+    isSearch,
+    isFilter,
+    size
+  );
+  getHTML(resultArray);
 };
 
 // function to search photos
 inputElem.addEventListener("change", () => {
-  search();
+  isSearch = true;
+  size = 10;
+  search(inputElem.value, isSearch);
+});
+
+clearButton.addEventListener("click", () => {
+  inputElem.value = "";
+  isSearch = false;
+  size = 10;
+  search(inputElem.value, isSearch);
 });
 
 // function to filter photos
-filterElem.addEventListener("change", () => filter(filterElem.value));
+filterElem.addEventListener("change", () => {
+  if (filterElem.value === "all") {
+    isFilter = false;
+    size = 10;
+    filter(filterElem.value, isFilter);
+  } else {
+    isFilter = true;
+    size = 10;
+    filter(filterElem.value, isFilter);
+  }
+});
