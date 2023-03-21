6720db77c5063bfd8f7488f600b697111d462093
nishu-murmu <nishumurmu017@gmail.com>
2023-03-21 16:18:07 +0530

    fixing again

--- album.js
+++ album.js
@@ -15,6 +15,7 @@ let loadMoreButton = document.querySelector("#load-more")
 let inputElem = document.getElementById("search-text")
 let filterElem = document.getElementById("filters")
 let clearButton = document.getElementById("clear")
+// import { debounce } from "./utilities/commonFunctions"
 
 const showSkeleton = () => {
   for (let i = 0; i < 10; ++i) {
@@ -26,11 +27,11 @@ const showSkeleton = () => {
   }
 }
 
-const getHTML = (arr, isAPIcall) => {
+const getHTML = (arr,filterArr, isAPIcall) => {
   if(!isAPIcall) showSkeleton()
   innerhtml = ""
 
-  arr.forEach((item) => {
+  filterArr.forEach((item) => {
     innerhtml += `
       <div class="blog-card" id="blog-card">
         <div class="blog-img">
@@ -60,6 +61,19 @@ const getHTML = (arr, isAPIcall) => {
     if(size !== 50) {
       loadMoreButton.classList.remove("hidden")
     }
+    console.log({size, len: arr.length, actualarr: arr, expectedarr: filterArr})
+    if(size === arr.length) {
+      loadMoreButton.classList.add("hidden")
+    }
+    if(size > arr.length) loadMoreButton.classList.add("hidden")
+    if (filterElem.value !== "all" && size === 50 && isLoadMore) {
+      document.querySelector("#limit").classList.remove("hidden")
+      loadMoreButton.classList.add("hidden")
+    }
+    if (filterArr.length === 0) {
+      loadMoreButton.classList.add("hidden")
+      blogSection.innerHTML = `<div class="no-data">No Data Found!</div>`
+    }
     if (isAPIcall) {
       document.querySelectorAll(".blog-card").forEach((item) => {
         item.classList.add("fadeIn")
@@ -77,7 +91,6 @@ const getFilters = (
   isFilter,
   limit
 ) => {
-  console.log({arr, inputValue, filtervalue, isLoadMore, isSearch, isFilter})
   arr = arr.filter((item) => {
     if (isFilter && isSearch) {
       if (filtervalue === "all") return item.title.toLowerCase().includes(inputValue.toLowerCase())
@@ -95,26 +108,13 @@ const getFilters = (
     }
     return item
   })
-  console.log(arr, 'arr')
-  arr = arr.slice(start, limit)
+  let newarr = arr.slice(start, limit)
 
-  if (filtervalue !== "all" && limit === 50 && isLoadMore) {
-    document.querySelector("#limit").classList.remove("hidden")
-    loadMoreButton.classList.add("hidden")
-  }
-  if (size > arr.length) {
-    loadMoreButton.classList.add("hidden")
-  }
-  if (arr.length == 10) {
-    document.querySelector("#limit").classList.add("hidden")
-    // loadMoreButton.classList.remove("hidden")
-  }
-
-  return arr
+  return {arr, newarr}
 }
 
 // functions
-async function getPhotos(inputValue, filterValue, isSearch, isFilter, isQuery) {
+async function getPhotos(inputValue, filterValue, isSearch, isFilter) {
   showSkeleton()
     await fetch("https://jsonplaceholder.typicode.com/photos")
       .then((res) => res.json())
@@ -130,45 +130,8 @@ async function getPhotos(inputValue, filterValue, isSearch, isFilter, isQuery) {
       isSearch,
       isFilter,
       size,
-      isQuery
     )
-
-    getHTML(resultArray, true)
-  setTimeout(() => {
-    if(resultArray.length == 0) {
-      loadMoreButton.classList.add("hidden")
-      blogSection.innerHTML = `<div class="no-data">No Data Found!</div>`
-    }
-    if(resultArray.length < size) {
-      loadMoreButton.classList.add("hidden")
-    }
-  }, 1000);
-}
-
-const search = (inputValue, isSearch) => {
-  const resultArray = getFilters(
-    totalList,
-    inputValue,
-    filterElem.value,
-    isLoadMore,
-    isSearch,
-    isFilter,
-    size
-  )
-  getHTML(resultArray)
-}
-
-const filter = (value, isFilter) => {
-  const resultArray = getFilters(
-    totalList,
-    inputElem.value,
-    value,
-    isLoadMore,
-    isSearch,
-    isFilter,
-    size
-  )
-  getHTML(resultArray)
+    getHTML(resultArray.arr,resultArray.newarr, true)
 }
 
 function debounce(func, timeout = 1000) {
@@ -202,7 +165,7 @@ window.onload = () => {
 
     if (inputElem.value) isSearch = true
     if (inputElem.value || filterElem.value) {
-      getPhotos(inputElem.value, filterElem.value, isSearch, isFilter, true)
+      getPhotos(inputElem.value, filterElem.value, isSearch, isFilter)
     } 
   } catch (err) {
     console.log(err)
@@ -222,14 +185,13 @@ loadMoreButton.onclick = () => {
     isFilter,
     size
   )
-  getHTML(resultArray)
+  getHTML(resultArray.arr, resultArray.newarr)
 }
 
 // function to search photos
 inputElem.addEventListener("keyup", () => {
   isSearch = true
   size = 10
-  search(inputElem.value, isSearch)
   processChange()
 })
 
@@ -239,7 +201,6 @@ clearButton.addEventListener("click", () => {
   inputElem.value = ""
   isSearch = false
   size = 10
-  search(inputElem.value, isSearch)
   getURLParams()
 })
 
@@ -248,11 +209,9 @@ filterElem.addEventListener("change", () => {
   if (filterElem.value === "all") {
     isFilter = false
     size = 10
-    filter(filterElem.value, isFilter)
   } else {
     isFilter = true
     size = 10
-    filter(filterElem.value, isFilter)
   }
   getURLParams()
 })
